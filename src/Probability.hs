69586ca81ac287694debc489b68ee2951a7b1859
Peter Taylor <info@emersont1.co.uk>
2021-04-08 14:49:25 +0100

    Added some more functions i developed in the black-jack branch

--- src/Probability.hs
+++ src/Probability.hs
@@ -1,7 +1,11 @@
-module Probability(module Probability.Internal, dice, normalise) where
+{-# LANGUAGE TupleSections #-}
+module Probability(module Probability.Internal, dice, normalise, rand, mix) where
     import Probability.Internal
     import Data.Bifunctor
     import Data.Ratio
+
+    rand :: [a] -> Probability a
+    rand  =  normalise . P . fmap (,1)
 
     dice :: Integer -> Probability Integer
     dice n = P  . zip [1 .. n] . repeat $ 1%n
@@ -9,3 +13,9 @@ module Probability(module Probability.Internal, dice, normalise) where
     normalise :: Probability a -> Probability a
     normalise (P x) = P (map (second (/s)) x)
         where s = foldl (\z (_, p)-> z + p) 0 x
+
+    mix :: [Probability a] -> Probability [a]
+    mix = foldr f (return [])
+        where f p cs = do c <- cs
+                          p' <- p
+                          return (p':c)
